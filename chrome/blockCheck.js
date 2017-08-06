bcae44c02aeba53d67979b78f0ee68b565131c4c
Danny Guo <dannyguo91@gmail.com>
2017-08-05 19:34:32 -0500

    Fix visiting a blocked page in the tab history
    This deals with the edge case where a page in the user's tab history
    becomes blocked due to the user turning PawBlock on or adding a new
    rule. If the user tries to navigate to the now blocked page (using back,
    forward, or even refresh), he or she would immediately be blocked again.
    With this fix, the page is actually replaced in the tab history with the
    PawBlock block, making it possible to go back.

--- chrome/blockCheck.js
+++ chrome/blockCheck.js
@@ -12,60 +12,83 @@ chrome.storage.sync.get({on: true}, function (items) {
   }
 });
 
-chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
+chrome.webNavigation.onCommitted.addListener(function (details) {
   // Avoid iFrame navigations
-  if (details.frameId === 0) {
-    var storageQuery = {
-      rules: [],
-      allowedTabId: -1,
-      on: true
-    };
-
-    chrome.storage.sync.get(storageQuery, function (items) {
-      var error = chrome.runtime.lastError;
-      if (error) {
-        console.error('Failed to get data from storage:', error);
-        return;
-      }
-
-      if (!items.on) {
-        return;
-      }
-
-      if (items.allowedTabId === details.tabId) {
-        chrome.storage.sync.remove('allowedTabId');
-        return;
-      }
-
-      var slashes = /^\/|\/$/g;
-      var targetUrl = new URL(details.url);
-      var targetDomain = targetUrl.hostname.replace(/^www\./, '');
-      var targetPath = targetUrl.pathname.replace(slashes, '').split('/');
-
-      items.rules.forEach(function (rule) {
-        if (targetDomain === rule.domain) {
-          var rulePath = rule.path.replace(slashes, '').split('/');
-
-          /* Only block the site if the target path includes all of
-             the rule path, which means that each segment matches up.
-
-             So with a rule of /foo/bar/baz, allow /foo/bar,
-             allow /foo/bar/bazs, block /foo/bar/baz, and block
-             /foo/bar/baz/qux. */
-          var i = 0;
-          while (i < targetPath.length && i < rulePath.length &&
-                 (rulePath[i] === '' || targetPath[i] === rulePath[i])) {
-            i++;
-          }
-
-          if (i === rulePath.length)  {
-            var blockedUrl = encodeURIComponent(details.url);
-            chrome.tabs.update(details.tabId, {
-              url: chrome.runtime.getURL('block.html?target=' + blockedUrl)
-            });
-          }
-        }
-      });
-    });
+  if (details.frameId !== 0) {
+    return;
   }
+
+  var storageQuery = {
+    rules: [],
+    allowedTabId: -1,
+    on: true
+  };
+
+  chrome.storage.sync.get(storageQuery, function (items) {
+    var error = chrome.runtime.lastError;
+    if (error) {
+      console.error('Failed to get data from storage:', error);
+      return;
+    }
+
+    if (!items.on) {
+      return;
+    }
+
+    if (items.allowedTabId === details.tabId) {
+      chrome.storage.sync.remove('allowedTabId');
+      return;
+    }
+
+    var slashes = /^\/|\/$/g;
+    var targetUrl = new URL(details.url);
+    var targetDomain = targetUrl.hostname.replace(/^www\./, '');
+    var targetPath = targetUrl.pathname.replace(slashes, '').split('/');
+
+    items.rules.forEach(function (rule) {
+      if (targetDomain === rule.domain) {
+        var rulePath = rule.path.replace(slashes, '').split('/');
+
+        /* Only block the site if the target path includes all of
+           the rule path, which means that each segment matches up.
+
+           So with a rule of /foo/bar/baz, allow /foo/bar,
+           allow /foo/bar/bazs, block /foo/bar/baz, and block
+           /foo/bar/baz/qux. */
+        var i = 0;
+        while (i < targetPath.length && i < rulePath.length &&
+               (rulePath[i] === '' || targetPath[i] === rulePath[i])) {
+          i++;
+        }
+
+        if (i === rulePath.length)  {
+          var blockedUrl = encodeURIComponent(details.url);
+          var blockPageUrl = chrome.runtime.getURL('block.html?target=' +
+                                                   blockedUrl)
+
+          /* Deal with the edge case of the user adding a rule or turning
+             PawBlock on but then trying to navigate backwards in history to
+             a now blocked page. The problem with just updating the tab url
+             is that it creates an infinite loop because navgiating to that
+             blocked page would trigger the PawBlock block, and clicking
+             "Never mind" would cause another navigation attempt to that
+             blocked page, and so on. The solution is to allow the navigation
+             to occur but to immediaately replace the page with the PawBlock
+             block in the tab's browser history. This solution also works
+             when there are multiple newly blocked pages in the history. */
+
+          // Try to avoid flashing the website before the redirect occurs
+          chrome.tabs.insertCSS(details.tabId, {
+            code: '* { display: none !important; }',
+            runAt: 'document_start'
+          });
+
+          chrome.tabs.executeScript(details.tabId, {
+            code: 'window.location.replace("' + blockPageUrl +'");',
+            runAt: 'document_start'
+          });
+        }
+      }
+    });
+  });
 });
